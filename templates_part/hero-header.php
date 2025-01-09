12bd4bf3211ed12d9f504b6393000a5bd7688e21
ZO DEV <zorofannumber1@gmail.com>
2025-01-09 17:33:09 +0100

    ajout h1 hero header et appel des photos

--- templates_part/hero-header.php
+++ templates_part/hero-header.php
@@ -7,6 +7,21 @@
         
     );
 
+      // Requête photo format paysage aléatoire
+      $args_banner = array(
+        'post_type' => 'Photo', 
+        'posts_per_page' => 1,
+        'orderby' => 'rand',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'formats',
+                'field' => 'slug',
+                'terms' => 'paysage',
+            ),
+        ),
+    );
+
+
     $banner = new WP_Query($args_hero);
 
     if ($banner->have_posts()) {
@@ -16,6 +31,14 @@
             the_post_thumbnail('large');
         }
         wp_reset_postdata();
-    } 
+    }  else {
+        echo '<p>Aucune photo trouvée.</p>';
+    }
+    ?>
+    
+    <!-- Conteneur pour le texte -->
+    <div class="hero-text">
+        <h1>Photographe Event</h1>
+    </div>
+    
 
-?>
